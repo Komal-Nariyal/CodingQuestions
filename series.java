8d0b4328efe5bfcd24a99cc583aa2939ffa304e0
komal-nariyal <komalnariyal@gmail.com>
2024-10-14 19:04:32 +0530

    Fibonacci series added

---
+++ series.java
@@ -0,0 +1,18 @@
+public class series {
+  public static void main(String[] args) {
+
+    int num = 15;
+    int a = 0, b = 1;
+
+    System.out.print(a + " , " + b + " , ");
+
+    int nextTerm;
+    for (int i = 2; i < num; i++) {
+      nextTerm = a + b;
+      a = b;
+      b = nextTerm;
+System.out.print(nextTerm + " , ");
+    }
+
+  }
+}
